394a39b489380537c446b9cc445bb541f22877ad
Evelien-M <32199993+Evelien-M@users.noreply.github.com>
2020-10-13 20:45:52 +0200

    Entity setup

--- Ninja_manager/Database/Tables/Inventory.sql
+++ Ninja_manager/Database/Tables/Inventory.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Inventory]
 (
-	[Id_Ninja] INT NOT NULL PRIMARY KEY, 
-    [Id_Gear] INT NOT NULL PRIMARY KEY, 
+	[Id_Ninja] INT NOT NULL, 
+    [Id_Gear] INT NOT NULL, 
+    PRIMARY KEY ([Id_Ninja], [Id_Gear]), 
     CONSTRAINT [FK_Inventory_Ninja] FOREIGN KEY ([Id_Ninja]) REFERENCES [Ninja]([Id]), 
     CONSTRAINT [FK_Inventory_Gear] FOREIGN KEY ([Id_Gear]) REFERENCES [Gear]([Id])
 )
