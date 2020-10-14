fca920aee3993fc03df2864919f13961a8cc3e5d
Evelien-M <32199993+Evelien-M@users.noreply.github.com>
2020-10-14 16:46:09 +0200

    mvvmlight setup

---
+++ Ninja_manager/Ninja_manager/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,80 @@
+/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:Ninja_manager"
+                           x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
+*/
+
+using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace Ninja_manager.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+
+            SimpleIoc.Default.Register<NinjaEditViewModel>();
+            SimpleIoc.Default.Register<NinjaListViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        public NinjaEditViewModel NinjaEdit
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<NinjaEditViewModel>();
+            }
+        }
+
+        public NinjaListViewModel NinjaList
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<NinjaListViewModel>();
+            }
+        }
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
